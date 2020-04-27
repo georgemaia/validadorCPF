c3662d5bfe6015d0b62801d908603517fb40bbc6
georgemaia <georgemaia@gmail.com>
2020-04-27 17:08:53 -0300

    exibir alerta

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# gama-no-javascript-basico
+# Validação de CPF via Javascript
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log('Javascript carregado!');
 
+function validaCPF(cpf) {
+    return false;
+}
+
 function validacao() {
     console.log('Iniciando a validação do CPF...');
+
+    var cpf = document.getElementById('cpf_digitado').value;
+    
+    var resultadoValidacao = validaCPF(cpf);
+
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
 }
