a43cc2023985eac8282a84da3e6e99c4816a1df1
georgemaia <georgemaia@gmail.com>
2020-04-27 18:13:47 -0300

    separa digitos

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 console.log('Javascript carregado!');
 
 function validaCPF(cpf) {
-    return false;
+
+    if(cpf.length != 11) {
+        return false;
+    } else {
+
+        var numeros = cpf.substring(0,9);   // pega os 9 primeiro dígitos
+        var digitos = cpf.substring(9);   // pega apenas a partir do 9 digito
+        
+        var soma = 0;
+        for( var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        console.log(soma);
+
+        return true;
+    }
 }
 
 function validacao() {
